74a6c77f6568cd57646ebfbe3cde84e446d004c8
Dmitry  Kutsenko <d2emonium@gmail.com>
2014-01-22 00:19:17 +0200

    Basic code refactoring

--- main.cpp
+++ main.cpp
@@ -11,24 +11,50 @@
 
 using namespace std;
 
+const int delayFireball =  2000;
+const int delaySdl      =  2000;
+const int delaySchism   = 10000;
+
+const char* logoFireball = "images/logo_fireball.bmp";
+const char* logoSdl      = "images/logo_sdl.bmp";
+const char* logoSchism   = "images/logo_schism.bmp";
+
+/**
+ * Showing  logo
+ * @param SDL_Surface* logo logo surface
+ * @param SDL_Surface* screen  screen surface
+ * @param int delay time to show logo
+ */
+int showLogo(SDL_Surface* screen,  const char* logo_filename, const int delay)
+{
+    SDL_Surface* logo = SDL_LoadBMP(logo_filename);
+
+    SDL_BlitSurface(logo, NULL, screen, NULL);
+    SDL_Flip(screen);
+    SDL_Delay(delay);
+
+    SDL_FreeSurface(logo);
+    
+    return 1;   
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-    SDL_Surface* hello  = NULL;
     SDL_Surface* screen = NULL;
-    
+
+    // Iitialization SDL
     SDL_Init(SDL_INIT_EVERYTHING);
-    screen = SDL_SetVideoMode(640, 480, 0, SDL_SWSURFACE);
-    hello  = SDL_LoadBMP("images/ready/logo_fireball.bmp");
-    
-    SDL_BlitSurface(hello, NULL, screen, NULL);
-    SDL_Flip(screen);
-    SDL_Delay(2000);
-    
-    SDL_FreeSurface(hello);
+    screen = SDL_SetVideoMode(800, 600, 0, SDL_SWSURFACE);
+
+    // Showing logos
+    showLogo(screen, logoFireball, delayFireball);
+    showLogo(screen, logoSdl,      delaySdl     );
+    showLogo(screen, logoSchism,   delaySchism  );
+
+    // Finalization
     SDL_Quit();
     
     return 0;
 }
-
