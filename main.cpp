c859777013611034081cede1f499253a3a317578
Dmitry  Kutsenko <d2emonium@gmail.com>
2014-01-22 01:56:44 +0200

    Basic  blitting test

--- main.cpp
+++ main.cpp
@@ -11,47 +11,131 @@
 
 using namespace std;
 
-const int delayFireball =  2000;
-const int delaySdl      =  2000;
-const int delaySchism   = 10000;
+const int SCREEN_WIDTH  = 800;
+const int SCREEN_HEIGHT = 600;
+const int SCREEN_BPP    = 32;
 
-const char* logoFireball = "images/logo_fireball.bmp";
-const char* logoSdl      = "images/logo_sdl.bmp";
-const char* logoSchism   = "images/logo_schism.bmp";
+const int DELAY_FIREBALL =  2000;
+const int DELAY_SDL      =  2000;
+const int DELAY_SCHISM   = 10000;
+const int DELAY_MAIN     = 10000;
+
+const char* WM_CAPTION  = "Схизма";
+
+const char* LOGO_FIREBALL = "images/logo_fireball.bmp";
+const char* LOGO_SDL      = "images/logo_sdl.bmp";
+const char* LOGO_SCHISM   = "images/logo_schism.bmp";
+const char* IMG_UNIT       = "images/unit.bmp";
+const char* IMG_BACKGROUND = "images/background.bmp";
+
+const int LOGO_FIREBALL_X = 0;
+const int LOGO_FIREBALL_Y = 0;
+const int LOGO_SDL_X = 0;
+const int LOGO_SDL_Y = 0;
+const int LOGO_SCHISM_X = 0;
+const int LOGO_SCHISM_Y = 0;
+
+const int UNIT_X = 180;
+const int UNIT_Y = 180;
+
+/**
+ * Loading image into SDL
+ * @param filename image file
+ * @return optimized image
+ */
+SDL_Surface* loadImage(const char* filename)
+{
+    SDL_Surface* loaded_image    = NULL;
+    SDL_Surface* optimized_image = NULL;
+    
+    loaded_image = SDL_LoadBMP(filename);
+    if(loaded_image != NULL)
+    {
+        optimized_image = SDL_DisplayFormat(loaded_image);
+        SDL_FreeSurface(loaded_image);
+    }
+    
+    return optimized_image;
+}
+
+/**
+ * Showing image
+ * @param screen screen surface
+ * @param x horizontal position
+ * @param y vertical position
+ * @param filename filename of the image
+ * @return errorcode
+ */
+int showImage(SDL_Surface* screen, int x, int y, const char* filename)
+{
+    SDL_Rect offset;
+    offset.x = x;
+    offset.y = y;
+ 
+    SDL_Surface* logo = NULL;
+ 
+    logo = SDL_LoadBMP(filename);
+    SDL_BlitSurface(logo, NULL, screen, &offset);
+    SDL_FreeSurface(logo);
+
+    return 0;   
+}
 
 /**
  * Showing  logo
- * @param SDL_Surface* logo logo surface
- * @param SDL_Surface* screen  screen surface
- * @param int delay time to show logo
+ * @param screen screen surface
+ * @param x horizontal position
+ * @param y vertical position
+ * @param filename image filename
+ * @param delay time to show logo
+ * @return errorcode
  */
-int showLogo(SDL_Surface* screen,  const char* logo_filename, const int delay)
+int showLogo(SDL_Surface* screen, int x, int y, const char* filename, const int delay)
 {
-    SDL_Surface* logo = SDL_LoadBMP(logo_filename);
+    showImage(screen, x, y, filename);
 
-    SDL_BlitSurface(logo, NULL, screen, NULL);
-    SDL_Flip(screen);
+    if(SDL_Flip(screen) == -1)
+    {
+        return 1;
+    }
+
     SDL_Delay(delay);
-
-    SDL_FreeSurface(logo);
+    //SDL_FreeSurface(logo);
     
-    return 1;   
+    return 0;   
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    SDL_Surface* screen = NULL;
+    SDL_Surface* screen     = NULL;
 
     // Iitialization SDL
-    SDL_Init(SDL_INIT_EVERYTHING);
-    screen = SDL_SetVideoMode(800, 600, 0, SDL_SWSURFACE);
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+    {
+        return 1;
+    }
+    screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+    if(!screen)
+    {
+        return 1;
+    }
+    SDL_WM_SetCaption(WM_CAPTION, NULL);
 
     // Showing logos
-    showLogo(screen, logoFireball, delayFireball);
-    showLogo(screen, logoSdl,      delaySdl     );
-    showLogo(screen, logoSchism,   delaySchism  );
+    showLogo(screen, LOGO_FIREBALL_X, LOGO_FIREBALL_Y, LOGO_FIREBALL, DELAY_FIREBALL);
+    showLogo(screen, LOGO_SDL_X,      LOGO_SDL_Y,      LOGO_SDL,      DELAY_SDL     );
+    showLogo(screen, LOGO_SCHISM_X,   LOGO_SCHISM_Y,   LOGO_SCHISM,   DELAY_SCHISM  );
+
+    // Showing main screen
+    showImage(screen,   0,   0, IMG_BACKGROUND);
+    showImage(screen, 180, 140, IMG_UNIT      );
+    if(SDL_Flip(screen) == -1)
+    {
+        return 1;
+    }
+    SDL_Delay(DELAY_MAIN);
 
     // Finalization
     SDL_Quit();
